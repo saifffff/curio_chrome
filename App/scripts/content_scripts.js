8f2e14b5600ac72e35fc9119a07ccca7c9945de9
saifffff <angryn1nj4@gmail.com>
2023-11-01 18:38:50 +0530

    day3 tasks complete

--- App/scripts/content_scripts.js
+++ App/scripts/content_scripts.js
@@ -6,10 +6,9 @@ const curioBtn = document.createElement("button");
 curioBtn.setAttribute("id","curioBtn");
 curioBtn.textContent = "C";
 
-curioBtn.addEventListener("click",setToggle);
+curioBtn.addEventListener("click",GetSpeech);
 // adding to dom
 document.body.appendChild(curioBtn);
-
 
 
 
@@ -18,11 +17,46 @@ function setToggle(){
     if(curioBtn.hasAttribute("toggle")===false){
         curioBtn.setAttribute("toggle",true);
         console.log("i am listening");
+
         }else{
         curioBtn.removeAttribute("toggle");
         console.log("not listening now");
         }
 }
+
+
+// speech recognition 
+function GetSpeech(){
+    const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
+   
+    let recognition = new SpeechRecognition();
+    recognition.onstart = () =>{
+        setToggle();
+    }
+
+    recognition.onspeechend = () => {
+        recognition.stop();
+        setToggle();
+    }
+
+    recognition.onresult = (result) => {
+        let vocalInput = result.results[0][0].transcript;
+        const myPopup = new Popup({
+            id: "Curio",
+            title: "Here Is What You Said : ",
+            content:vocalInput,
+        });
+        myPopup.show();
+         console.log("Here is what you said \n",vocalInput);
+    }
+
+    recognition.start();
+
+}
+
+
+
+
 
 
 
